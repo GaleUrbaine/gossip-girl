9061a87ebebeb6ca7e4aa406a13d01af91864f5e
GaleUrbaine <bloody_spyke@hotmail.fr>
2019-02-07 11:50:03 +0100

    changements mineurs

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   #static pages
-  get 'welcome/:user_entry', to: 'welcome#show'
-  get 'team', to: 'static_pages#team'
-  get 'contact', to: 'static_pages#contact'
+  get 'welcome/:user_entry', to: 'welcome#show', as: 'bienvenue'
+  get 'team', to: 'static_pages#team', as: 'team'
+  get 'contact', to: 'static_pages#contact', as: 'contact'
 
   root 'static_pages#index'
   resources :gossips do
